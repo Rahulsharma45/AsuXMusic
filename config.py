611688d8ec38b4976f9515f1f97ada08d7a0761a
KSK OP <104455718+Rahulsharma45@users.noreply.github.com>
2022-11-07 23:59:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv()
 get_queue = {}
 
 
-API_ID = int(getenv("API_ID", "13600724")) 
-API_HASH = getenv("API_HASH", "ee59fd28d0d065c6b7d105082c6a0ba0")
+API_ID = int(getenv("API_ID", "10571256")) 
+API_HASH = getenv("API_HASH", "a33ded138ee2b56111ee7cd5d3355c38")
 ASS_HANDLER = list(getenv("ASS_HANDLER", "/").split())
 BOT_TOKEN = getenv("BOT_TOKEN")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
@@ -18,5 +18,5 @@ PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/c5952790fa8235f499749.jpg
 START_IMG = getenv("START_IMG","https://te.legra.ph/file/7fd1cecadabad0bf96733.jpg")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TheFriendGroup")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/KSKxBots")
-STRING_SESSION = getenv("STRING_SESSION")
+STRING_SESSION = getenv("STRING_SESSION", None)
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "2102097596").split()))
